d5f55b45a749785211766f6bd9c3ed8827245914
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2022-06-01 14:39:16 -0400

    include list of convertible data structures in package documentation

--- R/interplex-package.r
+++ R/interplex-package.r
@@ -4,11 +4,32 @@
 #'   different data structures.
 #'   
 
-#' @details This package helps interface between different topological data
-#'   analytic packages and workflows by converting simplicial complex objects
-#'   stored using different data structures. Each conversion is designed to
-#'   retain as much annotation as possible, whether of simplices or of the
-#'   complex.
+#' @details
+#'
+#' This package helps interface between different topological data analytic
+#' packages and workflows by converting simplicial complex objects stored using
+#' different data structures. Each conversion is designed to retain as much
+#' annotation as possible, whether of simplices or of the complex.
+#'
+#' The package supports conversions between simplicial complexes stored using
+#' the following data structures:
+
+#' * a complete list of simplices,
+#'   as stored in `cmplx` values of filtration objects
+#'   in [the TDA package][TDA::TDA-package]
+#' * an object of class 'Rcpp_SimplexTree' or 'simplextree'
+#'   as implemented
+#'   in [the simplextree package][simplextree::simplextree-package]
+#' * an 'igraph' object,
+#'   as implemented
+#'   in [the igraph package][igraph]
+#' * a 'network' object,
+#'   as implemented
+#'   in [the network package][network::network-package]
+
+#'
+#' When converting from a simplicial complex structure to a graph structure,
+#' only the 1-skeleton is converted; simplices of dimension > 1 are discarded.
 
 #' @docType package
 #' @name interplex
