42963ae9a85eb160e5ffb9ee8e11e9036daf4e9a
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2022-06-01 08:25:18 -0400

    add package documentation

---
+++ R/interplex-package.r
@@ -0,0 +1,15 @@
+#' @title **interplex** package
+#'
+#' @description This is a helper package to convert simplicial complexes between
+#'   different data structures.
+#'   
+
+#' @details This package helps interface between different topological data
+#'   analytic packages and workflows by converting simplicial complex objects
+#'   stored using different data structures. Each conversion is designed to
+#'   retain as much annotation as possible, whether of simplices or of the
+#'   complex.
+
+#' @docType package
+#' @name interplex
+NULL
